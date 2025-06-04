b4dbb11e0c3a7a6655afcfe741a6db98f0afafbc
Isaac Kim <isaackim@nomad.local>
2025-06-04 03:01:54 -0400

    prettier format

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css';
 import Header from './components/Header';
-import Main from './components/mainSection'
+import Main from './components/mainSection';
 import { useState } from 'react';
 
 function App() {
-
   //currently setting the variable to false, as it is not in Light Mode
   const [isLightMode, setIsLightMode] = useState(false);
 
   const toggleTheme = () => {
-    setIsLightMode(prev => !prev);
-  }
-
+    setIsLightMode((prev) => !prev);
+  };
 
   return (
     <div className={isLightMode ? 'light' : 'dark'}>
