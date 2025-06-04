877de02549151930fe1f4cbd4ad4797c070b0ae0
Rens <rens@uplink.sh>
2025-06-04 02:21:13 +0200

    Added a theme toggle in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,29 @@
+import { CgDarkMode } from 'react-icons/cg';
+
 const Header = () => {
   return (
     <header>
       <div className="container navbar">
         <h1 className="logo">Thinkbin</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Notes</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
+        <div className="nav-right">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Notes</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <CgDarkMode className="theme-toggle" />
+        </div>
       </div>
     </header>
   );
