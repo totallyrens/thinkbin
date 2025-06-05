67ebbcb273fc3887b1524fb08ba7732efc08a719
Rens <rens@uplink.sh>
2025-06-05 02:58:31 +0200

    Created Dashboard

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -5,7 +5,7 @@ const Note = () => {
     <div className="note-card">
       <h3>Note #1</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, rerum.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, rerum...
       </p>
     </div>
   );
